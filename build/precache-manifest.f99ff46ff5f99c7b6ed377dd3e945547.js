e0b5b84b91bce8087cbf9b56e2fad5a12f578ded
ngatttnl <62821812+ngatttnl@users.noreply.github.com>
2020-04-04 22:21:34 +0700

    code 0404
    ab

---
+++ build/precache-manifest.f99ff46ff5f99c7b6ed377dd3e945547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663733897eb4fa6b5534e2cb620064a8",
+    "url": "/ngattt/index.html"
+  },
+  {
+    "revision": "629d2b191e311034fd98",
+    "url": "/ngattt/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0062749024cf227f79f3",
+    "url": "/ngattt/static/js/2.d7155cad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ngattt/static/js/2.d7155cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629d2b191e311034fd98",
+    "url": "/ngattt/static/js/main.5f81ad71.chunk.js"
+  },
+  {
+    "revision": "7d6a836c517f6d107ee0",
+    "url": "/ngattt/static/js/runtime-main.27e9f22d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ngattt/static/media/logo.5d5d9eef.svg"
+  }
+]);
